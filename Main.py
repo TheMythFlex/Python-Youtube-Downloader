989b97379dbef46e64c4902ef913b26e477d29c1
Flex Tools <93522281+TheMythFlex@users.noreply.github.com>
2022-06-13 10:34:41 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import ctypes
 import sys
 import os
+
+import keyboard
+from pystyle import Colorate, Colors, Center, Write
 from pytube import YouTube, Playlist
 import colorama
 from pytube.contrib import playlist
@@ -8,37 +11,33 @@ from pytube.request import stream
 from termcolor import colored
 from colorama import Fore, Back, Style
 from youtubesearchpython import *
-import easygui
 import time
 import string
 import pytube
-import PyQt5
 import requests
+import msvcrt as m
 from pytube import Playlist
-colorama.init()
 
+if os.name == 'nt':
+    import msvcrt
+    import ctypes
 
-def logo():
-    print(colored('███████╗██╗     ███████╗██╗  ██╗    ████████╗ ██████╗  ██████╗ ██╗     ███████╗', 'red'))
-    print(colored('██╔════╝██║     ██╔════╝╚██╗██╔╝    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝', 'red'))
-    print(colored('█████╗  ██║     █████╗   ╚███╔╝        ██║   ██║   ██║██║   ██║██║     ███████╗', 'red'))
-    print(colored('██╔══╝  ██║     ██╔══╝   ██╔██╗        ██║   ██║   ██║██║   ██║██║     ╚════██║', 'red'))
-    print(colored('██║     ███████╗███████╗██ ╔╝██╗       ██║   ╚██████╔╝╚██████╔╝███████╗███████║', 'red'))
-    print(colored('╚═╝     ╚══════╝╚══════╝╚═╝  ╚═╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝', 'red'))
+    class _CursorInfo(ctypes.Structure):
+        _fields_ = [("size", ctypes.c_int),
+                    ("visible", ctypes.c_byte)]
 
-ctypes.windll.kernel32.SetConsoleTitleW("YouTube Downloader - Made by Flex#8629")
-time.sleep(0.5)
-print("\n")
+def hide_cursor():
+    if os.name == 'nt':
+        ci = _CursorInfo()
+        handle = ctypes.windll.kernel32.GetStdHandle(-11)
+        ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
+        ci.visible = False
+        ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
+    elif os.name == 'posix':
+        sys.stdout.write("\033[?25l")
+        sys.stdout.flush()
 
-logo()
-
-
-print(colored('\nselect [1] OR [2]', 'cyan'))
-print(colored('\n[1] YOUTUBE LINK', 'magenta'))
-print(colored('[2] SEARCH YOUTUBE', 'magenta'))
-
-useanswer = input("\nENTER TYPE : ").upper()
-
+hide_cursor()
 
 def on_progress(stream, chunk, bytes_remaining):
     total_size = stream.filesize
@@ -46,148 +45,224 @@ def on_progress(stream, chunk, bytes_remaining):
     liveprogress = (int)(bytes_downloaded / total_size * 100)
     print("Downloading... [%{}]".format(liveprogress), end="\r")
 
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
 
-if useanswer == "1":
-    print(colored('\nSELECTED [1]', 'cyan'))
-    print(colored('\n[S] SINGLE VIDEO', 'magenta'))
-    print(colored('[P] PLAYLIST', 'magenta'))
-    useanswer = input("\nPLAYLIST OR SINGLE : ").upper()
-
-
-if useanswer == "S":
-    print(colored('\nSELECTED [S]', 'cyan'))
-    print(colored('\n[A] Audio', 'magenta'))
-    print(colored('[v] VIDEO', 'magenta'))
-    useanswer = input("\nENTER TYPE : ").upper()
+def logo():
+    text = (Colorate.Vertical(Colors.red_to_black, """
 
 
 
-try:
-    if useanswer == "A":
-        url = str(input("\nYoutube video url : "))
-        youtube = YouTube(url)
-        youtube.register_on_progress_callback(on_progress)
-        print("\n")
-        print(colored("Downloading : {} Has Started.".format(youtube.title), "red"))
-        print("\n")
-        youtube.streams.get_audio_only().download()
+                              ██╗░░░██╗░█████╗░██╗░░░██╗████████╗██╗░░░██╗██████╗░███████╗
+                              ╚██╗░██╔╝██╔══██╗██║░░░██║╚══██╔══╝██║░░░██║██╔══██╗██╔════╝
+                              ░╚████╔╝░██║░░██║██║░░░██║░░░██║░░░██║░░░██║██████╦╝█████╗░░
+                              ░░╚██╔╝░░██║░░██║██║░░░██║░░░██║░░░██║░░░██║██╔══██╗██╔══╝░░
+                              ░░░██║░░░╚█████╔╝╚██████╔╝░░░██║░░░╚██████╔╝██████╦╝███████╗
+                              ░░░╚═╝░░░░╚════╝░░╚═════╝░░░░╚═╝░░░░╚═════╝░╚═════╝░╚══════╝
+                                   
+          
+            """, 1))
+    print(Center.XCenter(text))
 
-except Exception as e:
-    print(e)
-    input("")
+os.system("title YouTube Downloader - Made by Flex#8629")
 
+def main():
+    global youtube
+    try:
+        logo()
 
-try:
-    if useanswer == "V":
-        url = str(input("\nYoutube video url : "))
-        youtube = YouTube(url)
-        youtube.register_on_progress_callback(on_progress)
-        print("\n")
-        print(colored("Downloading : {} Has Started.".format(youtube.title), "red"))
-        print("\n")
-        youtube.streams.get_highest_resolution().download()
+        print(Center.XCenter("""[1] Youtube Link | [2] Search Youtube
 
-except Exception as e:
-    print(e)
-    input("")
+        """))
 
-if useanswer == "P":
-    print(colored('\nSELECTED [P]', 'cyan'))
-    print(colored('\n[A] Audio', 'magenta'))
-    print(colored('[v] VIDEO', 'magenta'))
-    useanswer = input("\nENTER TYPE : ").upper()
-    if useanswer == "V":
-        videoscounter = 0
-        p = Playlist(input("\nYoutube Playlist url : "))
-        for videocount in p.videos:
-            videoscounter += 1
-        videoindex = 0
-        for video in p.videos:
-            video.register_on_progress_callback(on_progress)
-            print("\n")
-            videoindex += 1
-            print(
-                colored(
-                    "Downloading: [{}/{}] | {} Videos Has Been Started.".format(videoindex, videoscounter, video.title),
-                    "red"))
-            print("\n")
-            video.streams.get_highest_resolution().download()
+        useranswer = m.getch()
+        useranswer = str(useranswer).replace("'", "")
+        useranswer = str(useranswer).replace("b", "")
 
-    if useanswer == "A":
+        if useranswer == "1":
+            os.system("title YouTube Downloader - Youtube Link")
+            clearConsole()
+            logo()
+            print(Center.XCenter("""[1] Single | [2] Playlist
 
-        videoscounter = 0
-        p = Playlist(input("\nYoutube Playlist url : "))
-        for videocount in p.videos:
-            videoscounter += 1
-        videoindex = 0
-        for video in p.videos:
-            video.register_on_progress_callback(on_progress)
-            print("\n")
-            videoindex += 1
-            print(
-                colored(
-                    "Downloading: [{}/{}] | {} Videos Has Been Started.".format(videoindex, videoscounter, video.title),
-                    "red"))
-            print("\n")
-            video.streams.get_audio_only().download()
+            """))
+            useranswer = m.getch()
+            useranswer = str(useranswer).replace("'", "")
+            useranswer = str(useranswer).replace("b", "")
+            if useranswer == "1":
+                os.system("title YouTube Downloader - Youtube Link/Single")
+                clearConsole()
+                logo()
+                print(Center.XCenter("""[1] Audio | [2] Video
 
+                """))
+                useranswer = m.getch()
+                useranswer = str(useranswer).replace("'", "")
+                useranswer = str(useranswer).replace("b", "")
+                if useranswer == "1":
+                    os.system("title YouTube Downloader - Youtube Link/Single/Audio")
+                    clearConsole()
+                    logo()
+                    url = str(input("\nYoutube video url: "))
+                    clearConsole()
+                    logo()
+                    youtube = YouTube(url)
+                    print(colored("Downloading : {} Has Started.".format(youtube.title), "red"))
+                    youtube.streams.get_audio_only().download()
+                    clearConsole()
+                    logo()
+                    print(colored("Download Finished.".format(youtube.title), "green"))
+                    input("")
+                else:
+                    pass
+                if useranswer == "2":
+                    os.system("title YouTube Downloader - Youtube Link/Single/Video")
+                    clearConsole()
+                    logo()
+                    url = str(input("\nYoutube video url: "))
+                    clearConsole()
+                    logo()
+                    youtube = YouTube(url)
+                    youtube.register_on_progress_callback(on_progress)
+                    print(colored("Downloading : {} Has Started.".format(youtube.title), "red"))
+                    youtube.streams.get_highest_resolution().download()
+                    print(colored("Download Finished.".format(youtube.title), "green"))
+                    input("")
+            else:
+                pass
+            if useranswer == "2":
+                os.system("title YouTube Downloader - Youtube Link/Playlist")
+                clearConsole()
+                logo()
+                print(Center.XCenter("""[1] Audio | [2] Video
 
+                        """))
+                useranswer = m.getch()
+                useranswer = str(useranswer).replace("'", "")
+                useranswer = str(useranswer).replace("b", "")
+                if useranswer == "1":
+                    os.system("title YouTube Downloader - Youtube Link/Playlist/Audio")
+                    clearConsole()
+                    logo()
+                    videoscounter = 0
+                    p = Playlist(input("\nYoutube Playlist url: "))
+                    clearConsole()
+                    logo()
+                    for videocount in p.videos:
+                        videoscounter += 1
+                    videoindex = 0
+                    for video in p.videos:
+                        video.register_on_progress_callback(on_progress)
+                        videoindex += 1
+                        print(
+                            colored(
+                                "Downloading: [{}/{}] | {} Videos Has Been Started.".format(videoindex, videoscounter,
+                                                                                            video.title),
+                                "red"))
+                        video.streams.get_audio_only().download()
+                    print(colored("Download Finished.".format(youtube.title), "green"))
+                    input("")
+                else:
+                    pass
+                if useranswer == "2":
+                    os.system("title YouTube Downloader - Youtube Link/Playlist/Video")
+                    clearConsole()
+                    logo()
+                    videoscounter = 0
+                    p = Playlist(input("\nYoutube Playlist url : "))
+                    clearConsole()
+                    logo()
+                    for videocount in p.videos:
+                        videoscounter += 1
+                    videoindex = 0
+                    for video in p.videos:
+                        video.register_on_progress_callback(on_progress)
+                        videoindex += 1
+                        print(
+                            colored(
+                                "Downloading: [{}/{}] | {} Videos Has Been Started.".format(videoindex, videoscounter,
+                                                                                            video.title),
+                                "red"))
+                        video.streams.get_highest_resolution().download()
+                    print(colored("Download Finished.".format(youtube.title), "green"))
+                    input("")
 
+        if useranswer == "2":
+            os.system("title YouTube Downloader - Search Youtube")
+            clearConsole()
+            logo()
+            print(Center.XCenter("""[1] Audio | [2] Video
 
+                            """))
+            useranswer = m.getch()
+            useranswer = str(useranswer).replace("'", "")
+            useranswer = str(useranswer).replace("b", "")
+            if useranswer == "1":
+                os.system("title YouTube Downloader - Search Youtube/Audio")
+                clearConsole()
+                logo()
+                search = VideosSearch(input('\nSearch Youtube: '))
+                clearConsole()
+                logo()
+                print(colored("\n[Use Ctrl+C / Ctrl+V, TO COPY AND PASTE URL]", 'red'))
+                print("\n")
+                link = 0
+                for video in search.result()['result']:
+                    link += 1
+                    print(colored(video['title'], 'green'))
+                    print(colored(video['link'], 'blue'))
+                    print("-----------------------------------------------")
+                    if link == 3:
+                        break
+                url = str(input("\nEnter video url : "))
+                clearConsole()
+                logo()
+                print("\n")
+                youtube = YouTube(url)
+                print(colored("Downloading : {} Has Started.".format(youtube.title), "red"))
+                youtube.streams.get_audio_only().download()
+                clearConsole()
+                logo()
+                print("\n")
+                print(colored("Download Finished.".format(youtube.title), "green"))
+                input("")
+            else:
+                pass
+            if useranswer == "2":
+                os.system("title YouTube Downloader - Search Youtube/Video")
+                clearConsole()
+                logo()
+                search = VideosSearch(input('\nSearch Youtube: '))
+                clearConsole()
+                logo()
+                print(colored("\n[Use Ctrl+C / Ctrl+V, TO COPY AND PASTE URL]", 'red'))
+                print("\n")
+                link = 0
+                for video in search.result()['result']:
+                    link += 1
+                    print(colored(video['title'], 'green'))
+                    print(colored(video['link'], 'blue'))
+                    print("-----------------------------------------------")
+                    if link == 3:
+                        break
+                url = str(input("\nEnter video url : "))
+                clearConsole()
+                logo()
+                print("\n")
+                youtube = YouTube(url)
+                print(colored("Downloading : {} Has Started.".format(youtube.title), "red"))
+                youtube.streams.get_highest_resolution().download()
+                clearConsole()
+                logo()
+                print("\n")
+                print(colored("Download Finished.".format(youtube.title), "green"))
+                input("")
 
-if useanswer == "2":
-    print(colored('\nSELECTED [2]', 'cyan'))
-    print(colored('\n[A] Audio', 'magenta'))
-    print(colored('[F] VIDEO', 'magenta'))
-    useanswer = input("\nENTER TYPE : ").upper()
-    if useanswer == "A":
+        input("")
+    except:
+        print(Fore.RED + "[+] Error, Try again")
 
-        search = VideosSearch(input('\nSEARCH YOUTUBE : '))
-
-        print(colored("\n[Use Ctrl+c / Ctrl+v to COPY AND PASTE URL]", 'red'))
-
-        time.sleep(1.5)
-        print("\n")
-        for video in search.result()['result']:
-
-            print(colored(video['title'], 'green'))
-            print(colored(video['link'], 'cyan'))
-        else:
-            print(colored('\n[Use Ctrl+c / Ctrl+v to COPY AND PASTE URL]', 'red'))
-            url = str(input("\nYoutube video url : "))  # have to include in if or it shows up in other downloads
-            youtube = YouTube(url)
-            youtube.register_on_progress_callback(on_progress)
-            print("\n")
-            print(colored("Downloading : {} Has Started.".format(youtube.title), "red"))
-            print("\n")
-            youtube.streams.get_audio_only().download()
-
-    if useanswer == "F":
-
-        search = VideosSearch(input('\nSEARCH YOUTUBE : '))
-
-        print(colored("\n[Use Ctrl+c / Ctrl+v to COPY AND PASTE URL]", 'red'))
-        time.sleep(1.5)
-
-        for video in search.result()['result']:
-            print("\n")
-            print(colored(video['title'], 'green'))
-            print(colored(video['link'], 'cyan'))
-        else:
-            print(colored('\n[Use Ctrl+c / Ctrl+v to COPY AND PASTE URL]', 'red'))
-
-            url = str(input("\nYoutube video url : "))  # have to include in if or it shows up in other downloads
-            youtube = YouTube(url)
-            youtube.register_on_progress_callback(on_progress)
-            print("\n")
-            print(colored("Downloading : {} Has Started.".format(youtube.title), "red"))
-            print("\n")
-            youtube.streams.get_highest_resolution().download()
-
-
-
-
-print("\n")
-print(colored('DOWNLOAD COMPLETED', 'green'))
-print("\n")
-input("Press Enter To Exit : ")
+main()
